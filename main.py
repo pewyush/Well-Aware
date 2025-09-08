a808468f36b838e92105c14fe6ecfaf109feb348
eewyush <piyush.cntc@gmail.com>
2025-09-08 16:59:55 +0530

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy as sc
+import yfinance as yf
+
+
+print("Sucessfully imported libraries")
+
