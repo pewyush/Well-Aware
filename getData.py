11e4e138ed249d45ee04624801094b27b3b02e3f
Harshit Batra <68419702+N1CK99925@users.noreply.github.com>
2025-09-10 16:00:07 +0530

    gitignore update

--- getData.py
+++ getData.py
@@ -1,8 +1,8 @@
 #  Yahoo Data
-# import yfinance as yf
+import yfinance as yf
 
-# data = yf.download("TCS.NS", start="2025-01-01", end="2025-09-01")
-# data.to_csv("TCS.NS.csv")
+data = yf.download("TCS.NS", start="2025-01-01", end="2025-09-01")
+data.to_csv("TCS.NS.csv")
 
 
 
