df72b0c463e007023155e673a08482b39370b9a6
Harshit Batra <68419702+N1CK99925@users.noreply.github.com>
2025-09-21 00:41:19 +0530

    Reshaped the project Structure

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import scipy as sc
-import yfinance as yf
-
-
-print("Sucessfully imported libraries")
--- src/getData.py
+++ src/getData.py
@@ -1,4 +1,3 @@
-#  Yahoo Data
 import yfinance as yf
 
 data = yf.download("TCS.NS", start="2025-01-01", end="2025-09-01")
