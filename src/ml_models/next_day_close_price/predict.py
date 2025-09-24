bf83566d454e84f9d2b5e2e6138c1e038fc140ec
Harshit Batra <68419702+N1CK99925@users.noreply.github.com>
2025-09-24 09:17:19 +0530

    made model work

--- src/ml_models/next_day_close_price/predict.py
+++ src/ml_models/next_day_close_price/predict.py
@@ -1,6 +1,9 @@
 import joblib
 import os
 import pandas as pd
+from pathlib import Path
+
+parent_root = Path.cwd().parent
 
 MODEL_DIR = os.path.join(os.path.dirname(__file__), "models")
 FEATURES = ["Close","High","Low","Open","Volume","daily_return","vol_20","sentiment"]
@@ -10,12 +13,12 @@ def load_models():
     models = {}
     for f in os.listdir(MODEL_DIR):
         if f.endswith(".pkl"):
-            ticker = f.replace(".pkl", "")
+            ticker = f.replace("_rf.pkl", "")
             models[ticker] = joblib.load(os.path.join(MODEL_DIR, f))
     return models
 
-def predict_next_day_close(model, ticker, data_dir="data/processed/"):
+def predict_next_day_close(model, ticker, data_dir=parent_root/"data/processed/"):
     """Predict next-day close price using the last row of features"""
-    df = pd.read_csv(f"{data_dir}/{ticker}.csv")
+    df = pd.read_csv(f"{data_dir}/{ticker}.NS_prices_processed.csv")
     last_row = df[FEATURES].iloc[-1:]
     return model.predict(last_row)[0]
